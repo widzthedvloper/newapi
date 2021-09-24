982fc37388ed6abe396ed3b1bfccc0e2c2b5d4fd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-24 11:35:16 -0400

    Modify user's actions in the controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,37 +5,30 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    json_response(@users)
   end
 
   # GET /users/1
   def show
-    render json: @user
+    json_response(@user)
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user = User.create!(user_params)
+    json_response(@user, :created)
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user.update(user_params)
+    head :no_content
   end
 
   # DELETE /users/1
   def destroy
     @user.destroy
+    head :no_content
   end
 
   private
@@ -47,6 +40,6 @@ class Api::V1::UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:email)
+    params.permit(:email)
   end
 end
