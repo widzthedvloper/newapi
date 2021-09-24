10787231434d37dddd00001e9fa0cb425d15525b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-24 08:33:00 -0400

    Wrap routes around namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :users do
-      resources :foods do
-        resources :ingredients
-      end
-  end  
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+          resources :foods do
+            resources :ingredients
+          end
+      end  
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
