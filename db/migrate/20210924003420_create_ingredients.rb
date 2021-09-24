eb0d89e72c35b1be5fed6d776df5634da648d944
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-23 21:01:15 -0400

    Create ingredient model

--- db/migrate/20210924003420_create_ingredients.rb
+++ db/migrate/20210924003420_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration[6.1]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.number :calories
+      t.decimal :calories, precision: 10, scale: 2
       t.references :food, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_001005) do
+ActiveRecord::Schema.define(version: 2021_09_24_003420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2021_09_24_001005) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.decimal "calories", precision: 10, scale: 2
+    t.bigint "food_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_ingredients_on_food_id"
+  end
+
+  add_foreign_key "ingredients", "foods"
 end
