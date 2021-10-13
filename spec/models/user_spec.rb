a0fc22a67efc6455ba4ec25b67ba60a1cddde5bd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-10-13 15:03:10 -0400

    Fixing linter

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "Validations" do
+  context 'Validations' do
     subject { FactoryBot.build(:user) }
     it { should validate_presence_of(:email) }
   end
